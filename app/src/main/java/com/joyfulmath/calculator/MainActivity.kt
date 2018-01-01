d7ab549a1920a091f5cd50193009aab1f99782e8
demanluandyuki <demanluandyuki@126.com>
2018-01-01 22:10:01 +0800

    Symptom:remove unused modul Solution: RootCause:N/A Note:N/A

---
+++ app/src/main/java/com/joyfulmath/calculator/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.joyfulmath.calculator
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by deman.lu on 2018/1/1 0001.
+ */
+class MainActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
--- common/src/main/java/com/joyfulmath/common/mvp/IBaseView.kt
+++
@@ -1,8 +0,0 @@
-package com.joyfulmath.common.mvp
-
-/**
- * Created by deman.lu on 2017/12/31 0031.
- */
-interface IBaseView {
-    fun showError(msg:String)
-}
