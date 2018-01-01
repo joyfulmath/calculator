996e80cf6ad9da14b040864d706f2f66f4319283
demanluandyuki <demanluandyuki@126.com>
2018-01-01 22:46:56 +0800

    Symptom:add edit Solution: RootCause:N/A Note:N/A

--- app/src/main/java/com/joyfulmath/calculator/MainActivity.kt
+++ app/src/main/java/com/joyfulmath/calculator/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 
 /**
  * Created by deman.lu on 2018/1/1 0001.
+ *
  */
 class MainActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
